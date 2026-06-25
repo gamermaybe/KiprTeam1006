2aa019e824bf4e693244bc4acc63749776b15502
gamermaybe <249988761+gamermaybe@users.noreply.github.com>
2026-06-25 18:43:41 +0000

    Grabby now knocks over the stack of cubes, then turns, continuing to grab the top yellow cube and moving it onto the other pallet. then grabby backs up and goes towards the orange cone.

--- grabby.c
+++ grabby.c
@@ -1,43 +1,120 @@
 #include <kipr/wombat.h>
 
-int right= 3;
-int left= 0; 
+int ARM = 0;
+int CLAW = 1;
 
-// 2047 is the max the claw can close
-// 1320 is the max the claw can open (recommended
- 
-void claw_open_and_close() {
-   enable_servos();
-   set_servo_position(3,1320);
-   msleep(500);
-   set_servo_position(3,2047);
-   msleep(500);  
+int up = 0;
+int down = 564;
+int barely_down = 300;
+int closed = 2047;
+int open = 1333;
 
-}
+int LEFT = 3;
+int RIGHT = 0;
 
-int main(){
-  while (get_motor_position_counter(right) < 3000)
-    {
-      motor(right,100);
-      motor(left,100);
-      return 0;
+int main()
+{
+    enable_servos();
+    
+    set_servo_position(CLAW,closed);
+    set_servo_position(ARM,up);
+    msleep(500);
+    
+    clear_motor_position_counter(0);
+    clear_motor_position_counter(3);
+    
+    while (get_motor_position_counter(LEFT) < 1200) {
+		motor(LEFT,40);
+        motor(RIGHT,-40);
     }
-    claw_open_and_close();
+    ao();
+    
+    set_servo_position(CLAW,closed);
+    set_servo_position(ARM,up);
+    msleep(500);
+    
+    clear_motor_position_counter(0);
+    clear_motor_position_counter(3);
+    
+    while(get_motor_position_counter(LEFT) > -600) {
+    	motor(LEFT,-100);
+        motor(RIGHT,-100);
+    }
+    
+    clear_motor_position_counter(0);
+    clear_motor_position_counter(3);
+    
+    while (get_motor_position_counter(LEFT) > -1200) {
+		motor(LEFT,-40);
+        motor(RIGHT,40);
+    }
+    clear_motor_position_counter(0);
+    clear_motor_position_counter(3);
+    
+    while (get_motor_position_counter(LEFT) < 300) {
+		motor(LEFT,95);
+        motor(RIGHT,100);
+    }
+    ao();
+    
+    set_servo_position(CLAW,open);
+    msleep(500);
+    
+    clear_motor_position_counter(0);
+    clear_motor_position_counter(3);
+    
+    while (get_motor_position_counter(LEFT) < 200) {
+		motor(LEFT,98);
+        motor(RIGHT,100);
+    }
+    ao();
+    
+    set_servo_position(ARM,barely_down);
+    msleep(1000);
+    set_servo_position(CLAW,closed);
+    msleep(1000);
+    set_servo_position(ARM,up);
+    msleep(500);
+    
+    clear_motor_position_counter(0);
+    clear_motor_position_counter(3);
+    
+    while (get_motor_position_counter(LEFT) < 1200) {
+		motor(LEFT,98);
+        motor(RIGHT,100);
+    }
+    ao();
+    msleep(500);
+    set_servo_position(CLAW,open);
+    msleep(500);
+    
+    //
+    
+    clear_motor_position_counter(0);
+    clear_motor_position_counter(3);
+    
+    while(get_motor_position_counter(LEFT) > -2000) {
+    	motor(LEFT,-100);
+        motor(RIGHT,-100);
+    }
+    
+    clear_motor_position_counter(0);
+    clear_motor_position_counter(3);
+    
+    while(get_motor_position_counter(RIGHT) < 600) {
+    	motor(RIGHT, 100);
+        motor(LEFT, -100);
+    }
+    
+    clear_motor_position_counter(0);
+    clear_motor_position_counter(3);
+    
+    while(get_motor_position_counter(RIGHT) < 4000) {
+    	motor(RIGHT, 80);
+        motor(LEFT, 100);
+    }
+    
+    set_servo_position(ARM,down);
+    msleep(500);
     
 }
-
-// This was for the mini video (in the google slides
-
-//void servo_extend()
-//{
-    //enable_servos(); // turns on all servos
-    //set_servo_position(1,1216); // this sets the servo position to 1216 while on servo 1  
-    //msleep(500); // sets how long it does 
-    //set_servo_position(1,649); // this sets the servo position to 649 while on servo 1
-    //msleep(500); // sets how long it does
-    //set_servo_position(1,1216);  
-    //msleep(500); // sets how long it does
-    //set_servo_position(1,649); 
-    //msleep(500); // sets how long it does
-    //ao(); // turns off all servos
-//}
