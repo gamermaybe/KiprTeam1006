0ae2208e6ffad2845245cfda96869e2248fe7165
gamermaybe <249988761+gamermaybe@users.noreply.github.com>
2026-06-25 20:16:12 +0000

    More reliable going up and down the ramp, etc.

--- bowly.c
+++ bowly.c
@@ -1,23 +1,68 @@
 #include <kipr/wombat.h>
 
-int right=0;
-int left=3;
+int LEFT = 3;
+int RIGHT = 0;
+int ARM = 0;
 
-void push_balls(){
-    motor(right,96);
-    motor(left,100);
-    msleep(4000);
-}
+int UP = 40;
+int RELEASE = 1024;
 
-void go_backwards(){
-    (right,-100);
-    (left, -100);
+int main()
+{
+    //arm up
+    enable_servos();
+    set_servo_position(ARM, UP);
     msleep(500);
+    //push poms down ramp
+    enable_servos();
+    set_servo_position(ARM, UP);
+    msleep(500);
+    set_servo_position(ARM, RELEASE);
+    msleep(500);
+    
+    clear_motor_position_counter(RIGHT);
+    clear_motor_position_counter(LEFT);
+    while (get_motor_position_counter(0) < 9000) {
+		motor(LEFT,90);
+        motor(RIGHT,89);
+    }
+    clear_motor_position_counter(RIGHT);
+    clear_motor_position_counter(LEFT);
+    while (get_motor_position_counter(0) < 5000) {
+		motor(LEFT,90);
+        motor(RIGHT,80);
+    }
+    //go back up
+    clear_motor_position_counter(RIGHT);
+    clear_motor_position_counter(LEFT);
+    while (get_motor_position_counter(0) > -10000) {
+		motor(LEFT,-90);
+        motor(RIGHT,-80);
+    }
+    clear_motor_position_counter(RIGHT);
+    clear_motor_position_counter(LEFT);
+    while (get_motor_position_counter(0) > -5000) {
+		motor(LEFT,-87);
+        motor(RIGHT,-90);
+    }
+    //push block off platform
+    clear_motor_position_counter(RIGHT);
+    clear_motor_position_counter(LEFT);
+    while (get_motor_position_counter(0) < 4000) {
+		motor(LEFT,90);
+        motor(RIGHT,88);
+    }
+    clear_motor_position_counter(RIGHT);
+    clear_motor_position_counter(LEFT);
+    while (get_motor_position_counter(0) < 5000) {
+		motor(LEFT,91);
+        motor(RIGHT,88);
+    }
+    //go back again
+    clear_motor_position_counter(RIGHT);
+    clear_motor_position_counter(LEFT);
+    while (get_motor_position_counter(0) > -9000) {
+		motor(LEFT,-90);
+        motor(RIGHT,-88);
+    }
 }
-
-int main(){
-    push_balls();
-}
-
-
-
