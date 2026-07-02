1c1ee7dffba2a3cb2e753de21eb4e9e98c9efb2b
gamermaybe <249988761+gamermaybe@users.noreply.github.com>
2026-07-02 10:43:45 -0700

    UPDATE grabby

--- grabby.c
+++ grabby.c
@@ -6,6 +6,7 @@ int CLAW = 1;
 int up = 0;
 int down = 564;
 int barely_down = 300;
+int very_down = 800;
 int closed = 2047;
 int open = 1333;
 
@@ -88,12 +89,10 @@ int main()
     set_servo_position(CLAW,open);
     msleep(500);
     
-    //
-    
     clear_motor_position_counter(0);
     clear_motor_position_counter(3);
     
-    while(get_motor_position_counter(LEFT) > -2000) {
+    while(get_motor_position_counter(LEFT) > -2000) { 
     	motor(LEFT,-100);
         motor(RIGHT,-100);
     }
@@ -101,20 +100,90 @@ int main()
     clear_motor_position_counter(0);
     clear_motor_position_counter(3);
     
-    while(get_motor_position_counter(RIGHT) < 600) {
-    	motor(RIGHT, 100);
-        motor(LEFT, -100);
+    set_servo_position(ARM,very_down);
+    msleep(500);
+    
+    while(get_motor_position_counter(RIGHT) < 5) {
+    	motor(RIGHT, 75);
+        motor(LEFT, -75);
     }
     
     clear_motor_position_counter(0);
     clear_motor_position_counter(3);
     
-    while(get_motor_position_counter(RIGHT) < 4000) {
+    while(get_motor_position_counter(RIGHT) < 6000) {
     	motor(RIGHT, 80);
         motor(LEFT, 100);
     }
     
-    set_servo_position(ARM,down);
+    clear_motor_position_counter(0);
+    clear_motor_position_counter(3);
+    
+    while (digital (0) == 0 && digital (1) == 0){
+    	motor(LEFT,-60);
+        motor(RIGHT,-60);
+    }
+    while ((digital (0) == 0 && digital (1) == 1) || (digital (0) == 1 && digital (1) == 0)){
+    	motor(LEFT, -40);
+        motor(RIGHT, -40);
+    }
+    enable_servos();
+    
+    clear_motor_position_counter(0);
+    clear_motor_position_counter(3);
+    
+    set_servo_position(ARM,very_down);
+    msleep(500);
+    set_servo_position(CLAW,open);
     msleep(500);
     
+    while (get_motor_position_counter(LEFT) < 6500) {
+		motor(LEFT, 73);
+        motor(RIGHT, 75);
+    }
+    ao();
+    
+    clear_motor_position_counter(0);
+    clear_motor_position_counter(3);
+    
+    while (get_motor_position_counter(LEFT) < 750) {
+		motor(LEFT, 100);
+        motor(RIGHT, -100);
+    }
+    ao();
+    
+    clear_motor_position_counter(0);
+    clear_motor_position_counter(3);
+    
+    while (get_motor_position_counter(RIGHT) > -850) {
+		motor(LEFT, -100);
+        motor(RIGHT, -100);
+    }
+    ao();
+    clear_motor_position_counter(0);
+    clear_motor_position_counter(3);
+    
+    while (get_motor_position_counter(RIGHT) < 850) {
+		motor(LEFT, -100);
+        motor(RIGHT, 100);
+    }
+    ao();
+    
+    while (get_motor_position_counter(LEFT) < 850) {
+		motor(LEFT, 75);
+        motor(RIGHT, 75);
+    }
+    ao();
+    
+    set_servo_position(CLAW,closed);
+    msleep(500);
+    
+    clear_motor_position_counter(0);
+    clear_motor_position_counter(3);
+    
+    while (get_motor_position_counter(LEFT) < 850) {
+		motor(LEFT, 75);
+        motor(RIGHT, -75);
+    }
+    ao();
 }
